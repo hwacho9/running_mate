a3e12f5f67dda665f3c0ccd1d6faa72819eedeb7
hwacho9 <csh77776@gmail.com>
2024-10-28 19:16:12 +0900

    feat:add openstreetmap API

--- mobile/lib/screens/run_page.dart
+++ mobile/lib/screens/run_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class RunPage extends StatelessWidget {
   const RunPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Run Page'),
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(51.5, -0.09),
+          initialZoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            // Display map tiles from any source
+            urlTemplate:
+                'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+            userAgentPackageName: 'com.example.app',
+            // And many more recommended properties!
+          ),
+          RichAttributionWidget(
+            // Include a stylish prebuilt attribution widget that meets all requirments
+            attributions: [
+              TextSourceAttribution(
+                'OpenStreetMap contributors',
+              ),
+              // Also add images...
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
