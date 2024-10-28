9cec0197fb72cf685aada72918a4d7f240253bf7
hwacho9 <csh77776@gmail.com>
2024-10-28 19:07:11 +0900

    feat:bottom nav

--- mobile/lib/home_page.dart
+++ mobile/lib/home_page.dart
@@ -1,14 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:running_mate/screens/profile_page.dart';
+import 'package:running_mate/screens/run_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    RunPage(),
+    Center(child: Text('Stats Page')),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Running Mate')),
-      body: const Center(
-        child: Text('Welcome to Running Mate!'),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Stats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
       ),
     );
   }
---
+++ mobile/lib/screens/run_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RunPage extends StatelessWidget {
+  const RunPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Run Page'),
+    );
+  }
+}
