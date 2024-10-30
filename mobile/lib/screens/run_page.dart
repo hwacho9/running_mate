5d518e9de00c4df20f33dbd781f3a91930d1c973
hwacho9 <csh77776@gmail.com>
2024-10-30 14:18:00 +0900

    feat:get lat when touch & draw route

--- mobile/lib/screens/run_page.dart
+++ mobile/lib/screens/run_page.dart
@@ -2,36 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-class RunPage extends StatelessWidget {
+class RunPage extends StatefulWidget {
   const RunPage({super.key});
+
+  @override
+  _RunPageState createState() => _RunPageState();
+}
+
+class _RunPageState extends State<RunPage> {
+  List<LatLng> _routePoints = []; // 그린 경로를 저장하는 리스트
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FlutterMap(
         options: MapOptions(
-          initialCenter: LatLng(51.5, -0.09),
-          initialZoom: 13.0,
+          initialCenter: LatLng(34, 135), // 지도 중심 좌표
+          initialZoom: 13.0, // 줌 레벨 설정
+          onTap: _addPointToRoute, // 지도 터치 시 경로에 좌표 추가
         ),
         children: [
           TileLayer(
-            // Display map tiles from any source
             urlTemplate:
                 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
             userAgentPackageName: 'com.example.app',
-            // And many more recommended properties!
           ),
-          RichAttributionWidget(
-            // Include a stylish prebuilt attribution widget that meets all requirments
-            attributions: [
-              TextSourceAttribution(
-                'OpenStreetMap contributors',
+          PolylineLayer(
+            polylines: [
+              Polyline(
+                points: _routePoints, // 그린 경로의 좌표들
+                strokeWidth: 4.0,
+                color: Colors.red, // 경로의 색깔
               ),
-              // Also add images...
+            ],
+          ),
+          const RichAttributionWidget(
+            attributions: [
+              TextSourceAttribution('OpenStreetMap contributors'),
             ],
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _clearRoute,
+        child: const Icon(Icons.clear),
+      ),
     );
+  }
+
+  // TapPosition을 포함한 onTap 함수 수정
+  void _addPointToRoute(TapPosition tapPosition, LatLng latLng) {
+    setState(() {
+      _routePoints.add(latLng); // 터치한 위치의 좌표를 경로에 추가
+      print('Route points: $_routePoints');
+    });
+  }
+
+  // 경로 초기화
+  void _clearRoute() {
+    setState(() {
+      _routePoints.clear(); // 경로를 초기화
+    });
   }
 }
