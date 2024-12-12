66152fb0cd0b4d836d0f131df9a33856192664fb
hwacho9 <csh77776@gmail.com>
2024-12-13 00:08:08 +0900

    style:circle floadting button size, cicle

--- mobile/lib/widgets/Buttons/CircleFloatingActionButton.dart
+++ mobile/lib/widgets/Buttons/CircleFloatingActionButton.dart
@@ -6,6 +6,7 @@ class CircleFloatingActionButton extends StatelessWidget {
   final String tooltip;
   final Color backgroundColor;
   final Color iconColor;
+  final double size; // 버튼 크기 추가
 
   const CircleFloatingActionButton({
     super.key,
@@ -14,17 +15,28 @@ class CircleFloatingActionButton extends StatelessWidget {
     this.tooltip = '',
     this.backgroundColor = Colors.blue, // 기본 배경색
     this.iconColor = Colors.white, // 기본 아이콘 색
+    this.size = 56.0, // 기본 사이즈 (FloatingActionButton 기본 크기와 동일)
   });
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: onPressed,
-      tooltip: tooltip,
-      backgroundColor: backgroundColor,
-      child: Icon(
-        icon,
-        color: iconColor,
+    return ClipOval(
+      child: Material(
+        color: backgroundColor, // 버튼 배경색
+        child: InkWell(
+          onTap: onPressed,
+          child: SizedBox(
+            width: size,
+            height: size,
+            child: Center(
+              child: Icon(
+                icon,
+                color: iconColor,
+                size: size * 0.5, // 아이콘 크기는 버튼 크기에 비례
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
